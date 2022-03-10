a2cad3a26e4b673bf85188ae34d739e9113d525b
Yash Pandey (YP) <yash.btech.cs19@iiitranchi.ac.in>
2022-03-10 16:02:50 +0530

    lab4

--- src/python/Lab4/__main__.py
+++ src/python/Lab4/__main__.py
@@ -7,8 +7,8 @@ def main():
     parallel_runtime = scaper.scrape_parallelly()
 
     print(f"---------- Results ----------")
-    print(f"Sequential Runtime:\t {sequential_runtime}")
-    print(f"Parallel Runtime:\t {parallel_runtime}")
+    print(f"Sequential Runtime:\t {sequential_runtime} s")
+    print(f"Parallel Runtime:\t {parallel_runtime} s")
 
 if __name__ == "__main__":
     main()
